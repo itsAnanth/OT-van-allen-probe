12a11e6e785ea3190cf9b37757dcd222ed84e712
Ananth <ananthsankar2003@gmail.com>
2025-10-17 13:44:31 +0530

    Refactor train.py to add argument parsing and training
    Refactored training script to include argument parsing and model training logic.

--- scripts/train.py
+++ scripts/train.py
@@ -1,77 +1,69 @@
-import os
-import numpy as np
-from common.utils import load_pickle
-from torch.utils.data import TensorDataset, DataLoader
+import torch
+import torch.nn as nn
+import argparse
+from models.lstm import FluxLSTM
+from scripts.dataset import load_data
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train Models")
+    parser.add_argument("--lr", type=float, default=1e-03)
+    parser.add_argument("--max_epochs", type=int, default=10)
+    parser.add_argument("--data_path", type=str, default='../processed_data')
+    parser.add_argument("--seq_length", type=int, default=500)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument(
+    "--positional_features",
+    nargs="+",
+    default=["ED_R_OP77Q_intxt", "ED_MLAT_OP77Q_intxt", "ED_MLT_OP77Q_intxt_sin", "ED_MLT_OP77Q_intxt_cos"],
+    help="List of positional feature names"
+)
+
+    parser.add_argument(
+        "--time_series_prefixes",
+        nargs="+",
+        default=["AE_INDEX", "flow_speed", "SYM_H", "Pressure"],
+        help="List of time series variable prefixes"
+    )
+    args, remaining_args = parser.parse_known_args()
+    assert remaining_args == [], remaining_args
+    return parser, args
+
+def train(loader, args):
+    input_size = X.shape[2]  # number of features (excluding target flux)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = FluxLSTM(input_size, args.hidden_size).to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+    print(f"Using device: {device}")
 
 
-extract_dir = '../processed_data'
+    for epoch in range(args.max_epochs):
+        model.train()
+        epoch_loss = 0
+        for xb, yb in train_loader:
+            xb = xb.to(device)
+            yb = yb.to(device)
 
+            optimizer.zero_grad()
+            pred = model(xb)
+            loss = criterion(pred, yb)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item() * xb.size(0)
 
-def load_data(args):
-    # load pickle files
-    extract_dir = args.data_path
-    X_train = load_pickle(os.path.join(extract_dir, 'X_train_norm.pkl'))
-    y_train = load_pickle(os.path.join(extract_dir, 'y_train.pkl'))
-
-    X_val = load_pickle(os.path.join(extract_dir, 'X_val_norm.pkl'))
-    y_val = load_pickle(os.path.join(extract_dir, 'y_val.pkl'))
-
-    #Out of sample storm data
-    X_test_storm = load_pickle(os.path.join(extract_dir, 'X_test_storm_norm.pkl'))
-    y_test_storm = load_pickle(os.path.join(extract_dir, 'y_test_storm.pkl'))
-
+        epoch_loss /= len(train_loader.dataset)
+        print(f"Epoch {epoch+1}/{EPOCHS}, Loss: {epoch_loss:.4f}")
+        
     
-    # extract relevant features
-    positional_features = args.positional_features
-    time_series_prefixes = args.time_series_prefixes
-    
-    columns_to_keep = []
 
-    columns_to_keep.extend(positional_features)
-
-    for prefix in time_series_prefixes:
-        columns_to_keep.append(f"{prefix}_t_0")
-
-    X_train_selected = X_train[columns_to_keep]
-    X_val_selected = X_val[columns_to_keep]
-    X_test_storm_selected = X_test_storm[columns_to_keep]
+def main():
+    parser, args = parse_args()
     
-    data = [(X_train_selected, y_train), (X_val_selected, y_val), (X_test_storm_selected, y_test_storm)]
-    
-    data_loaders = [get_dataloader(args, X, y) for X, y in data]
-    return data_loaders
+    train_loader, val_loader, test_loader = load_data(args)
     
     
-def get_dataloader(args, X, y):
-    SEQ_LENGTH = args.seq_length  # sequence window size
-    BATCH_SIZE = args.batch_size
-
-    # 1. Prepare sequences
-    def create_sequences(x_df, y_df, seq_length):
-        xs = []
-        ys = []
-        for i in range(len(x_df) - seq_length):
-            if (i % 10000 == 0):
-                print(f"{i}/{len(x_df) - seq_length}")
-            x = x_df.iloc[i:i+seq_length, :]  # all features except target flux column
-            y = y_df.iloc[i+seq_length]     # flux value at next timestep
-            xs.append(x)
-            ys.append(y)
-        return np.array(xs), np.array(ys)
-
-
-    X, y = create_sequences(X, y, SEQ_LENGTH)
-
-    # Convert to torch tensors
-    X = torch.tensor(X, dtype=torch.float32)
-    y = torch.tensor(y, dtype=torch.float32)
-
-    # Dataset and DataLoader
-
-    dataset = TensorDataset(X, y)
-    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-    return loader
-
-
-
+if __name__ == "__main__":
+    main()
     
