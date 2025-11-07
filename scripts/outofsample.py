ed874843fc1e665336148c0dfc57fdb444492db7
itsAnanth <ananthsankar2003@gmail.com>
2025-11-07 07:05:58 +0000

    feat: out of sample testing script

--- scripts/outofsample.py
+++ scripts/outofsample.py
@@ -12,17 +12,19 @@ from common.utils import autodetect_device, set_random_seed
 if __name__ == "__main__":
     set_random_seed(42)
     parser = argparse.ArgumentParser(description="Test Model on out o fsample data")
-    parser.add_argument("--model-path", type=str, default=None)
+    parser.add_argument("--checkpoint-dir", type=str, default=None)
+    parser.add_argument("--model", type=str, default=None)
+
 
     args, remaining_args = parser.parse_known_args()
 
     assert remaining_args == [], remaining_args
+    assert args.checkpoint_dir is not None, "Invalid model path"
+    assert args.model is not None, "Invalid model name"
 
-    if not args.model_path:
-        raise ValueError("invalid model path")
 
 
-    config = load_config(args.model_path)
+    config = load_config(args.checkpoint_dir)
 
     train_loader, val_loader, test_loader = load_data(config)
 
@@ -30,7 +32,7 @@ if __name__ == "__main__":
 
     model = FluxLSTM(input_size, config.hidden_size).to(config.device)
 
-    model.load_state_dict(torch.load(os.path.join(config.checkpoint_dir, '54kev_11.pth'), map_location=config.device)['model_state_dict'])
+    model.load_state_dict(torch.load(os.path.join(config.checkpoint_dir, args.model), map_location=config.device)['model_state_dict'])
 
     print(evaluate(model, test_loader, config))
 
