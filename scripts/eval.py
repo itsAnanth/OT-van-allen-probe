4e3979fac99cf5f5b23ee3cfde48c1bb0a7de0fa
itsAnanth <ananthsankar2003@gmail.com>
2025-11-07 07:06:26 +0000

    feat: add validation losses

--- scripts/eval.py
+++ scripts/eval.py
@@ -4,13 +4,15 @@ from sklearn.metrics import r2_score
 
 # evaluate model
 
-def evaluate(model, loader, args):
+def evaluate(model, loader, args, criterion):
     device = torch.device(f'cuda:{args.gpu}' if torch.cuda.is_available() else 'cpu')
     
     model.eval()
 
     all_preds = []
     all_targets = []
+    total_loss = 0.0
+    num_batches = 0
 
     with torch.no_grad():
         for X_batch, y_batch in loader:
@@ -18,6 +20,11 @@ def evaluate(model, loader, args):
             X_batch = X_batch.to(device)
             y_batch = y_batch.to(device)
             outputs = model(X_batch)
+
+            loss = criterion(outputs, y_batch)
+
+            total_loss += loss.item()
+            num_batches += 1
 
             # Ensure both preds and targets are 1D numpy arrays
             all_preds.append(outputs.view(-1).cpu().numpy())
@@ -29,7 +36,8 @@ def evaluate(model, loader, args):
 
     # R² score
     r2 = r2_score(all_targets, all_preds)
+    avg_loss = total_loss / num_batches
         
     model.train()
 
-    return r2
+    return r2, avg_loss
